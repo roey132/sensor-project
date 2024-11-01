6aa45b110b64cd343f71fba5b8ffa1c6ab811560
roey132 <roey132@gmail.com>
2024-11-01 15:17:12 +0200

    first init

---
+++ main.py
@@ -0,0 +1,7 @@
+import sensors.ICM20948 #Gyroscope/Acceleration/Magnetometer
+import sensors.MPU925x #Gyroscope/Acceleration/Magnetometer
+import sensors.BME280   #Atmospheric Pressure/Temperature and humidity
+import sensors.LTR390   #UV
+import sensors.TSL2591  #LIGHT
+import sensors.SGP40
+import smbus
