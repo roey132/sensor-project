aaa124255747113bb9c7ec0088351628b3ee9458
roey132 <roey132@gmail.com>
2024-11-11 19:32:54 +0200

    add .env to project

--- kafka_topic_creator.py
+++ kafka_topic_creator.py
@@ -1,15 +1,13 @@
-from sys import api_version
 from kafka.admin import KafkaAdminClient, NewTopic
 
 # Set up Kafka Admin Client
 admin_client = KafkaAdminClient(
-    bootstrap_servers="10.100.102.49:9092",  # Replace with your Docker container IP and port if necessary
-    client_id="sensor_client",
-    api_version=(7,7,1)
+    bootstrap_servers="10.0.0.13:29092",  # Replace with correct IP and port if necessary
+    client_id="sensors"
 )
 
 # Define new topic
-topic_list = [NewTopic(name="sensors", num_partitions=1, replication_factor=1)]
+topic_list = [NewTopic(name="sensorstest", num_partitions=1, replication_factor=1)]
 
 # Create topic
 admin_client.create_topics(new_topics=topic_list, validate_only=False)
--- main.py
+++ main.py
@@ -4,11 +4,17 @@ import sensors.LTR390 as LTR390  #UV
 import sensors.TSL2591 as TSL2591 #LIGHT
 import sensors.SGP40 as SGP40
 import smbus2
+import time
 from dataclasses import dataclass, asdict
 from kafka import KafkaProducer
 import json
 from datetime import datetime
 import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DOCKER_MACHINE_IP = os.getenv("DOCKER_MACHINE_IP")
 
 # Configure logging
 logging.basicConfig(
@@ -34,14 +40,14 @@ print("bme280 T&H I2C address:0X76")
 
 @dataclass
 class DataOutput():
-    msg_time: str
+    timestamp: str
     pressure: int
     temp: int
     hum: int
     lux:float
     uvs:int
 
-producer = KafkaProducer(bootstrap_servers="10.100.102.54:9092", request_timeout_ms = 10000)
+producer = KafkaProducer(bootstrap_servers=f"{DOCKER_MACHINE_IP}:29092", request_timeout_ms = 10000)
 try:
     while True:
         bme: list = bme280.readData()
@@ -54,7 +60,7 @@ try:
         uvs = uv.UVS()
 
         sensor_data = DataOutput(
-            msg_time= str(datetime.now()),
+            timestamp= datetime.now().isoformat(),
             pressure=pressure,
             temp= temp,
             hum= hum,
@@ -62,11 +68,12 @@ try:
             uvs=uvs
         )
 
-        future = producer.send(topic="sensor-input", value=json.dumps(asdict(obj=sensor_data)).encode('utf-8'))
+        future = producer.send(topic="sensors", value=json.dumps(asdict(obj=sensor_data)).encode('utf-8'))
         # producer.send(topic="sensors", value=json.dumps("testtest").encode('utf-8'))
         # result = future.get(timeout=10)  # Wait for confirmation
         print("Message sent:", sensor_data)
         # producer.send('sensors', value=b'Hello from Python producer')
         # producer.flush()
+        time.sleep(0.5)
 except KeyboardInterrupt:
     exit()
